fcd012457a8f3e6ba067c96dc729ca98dad1f61d
Isaac <isaac.castro.dev@gmail.com>
2023-04-24 20:42:35 -0600

    Eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  extends: [
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+    "airbnb",
+    "prettier",
+  ],
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/require-default-props": "off",
+    "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/no-array-index-key": "off",
+    "import/prefer-default-export": "off",
+    "no-param-reassign": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "consistent-return": "off",
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+    "react/jsx-filename-extension": "off",
+    "react/function-component-definition": "off",
+    "prettier/prettier": "warn",
+  },
+};
